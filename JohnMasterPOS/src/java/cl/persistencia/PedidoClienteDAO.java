601ab61a1958cc192ac4c79a1f9e879084485478
Hiigaaran <simo.moralesg@gmail.com>
2015-12-05 19:03:01 -0300

    Agregando archivos faltantes

---
+++ JohnMasterPOS/src/java/cl/persistencia/PedidoClienteDAO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.persistencia;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Simon
+ */
+public class PedidoClienteDAO {
+    private Connection cnx;
+
+    public PedidoClienteDAO(Connection cnx) {
+        this.cnx = cnx;
+    }
+    
+    
+}
