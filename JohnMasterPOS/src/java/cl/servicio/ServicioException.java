1e3d80c2c21657741c3951b711340660f5600ed5
Hiigaaran <simo.moralesg@gmail.com>
2015-12-05 13:38:14 -0300

    ServicioException creado

---
+++ JohnMasterPOS/src/java/cl/servicio/ServicioException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.servicio;
+
+/**
+ *
+ * @author Simon
+ */
+public class ServicioException extends Exception{
+    public ServicioException(String s) {
+        super(s);
+    }
+}
