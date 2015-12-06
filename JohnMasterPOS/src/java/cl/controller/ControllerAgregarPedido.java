df5538485a5e3c1758d28573f63575e2d5161fe9
Hiigaaran <simo.moralesg@gmail.com>
2015-12-06 19:00:37 -0300

    ControllerAgregarPedido creado

---
+++ JohnMasterPOS/src/java/cl/controller/ControllerAgregarPedido.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author Simon
+ */
+@WebServlet(name = "ControllerAgregarPedido", urlPatterns = {"/ControllerAgregarPedido"})
+public class ControllerAgregarPedido extends HttpServlet {
+
+    @Resource(mappedName = "jdbc/puntoventa")
+    private DataSource ds;
+
+    
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+    
+
+}
