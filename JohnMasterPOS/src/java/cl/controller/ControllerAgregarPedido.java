fd739e3d3ea7d3e4dbcdb7696a48030771f6f831
jordansilva1990 <jordan.silva@hotmail.es>
2015-12-06 20:29:55 -0300

    crear lista detalles

--- JohnMasterPOS/src/java/cl/controller/ControllerAgregarPedido.java
+++ JohnMasterPOS/src/java/cl/controller/ControllerAgregarPedido.java
@@ -6,12 +6,17 @@
 package cl.controller;
 
 import cl.dominio.Cliente;
+import cl.dominio.PedidoDetalle;
 import cl.dominio.Producto;
 import cl.servicio.JohnMasterService;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -56,6 +61,50 @@ public class ControllerAgregarPedido extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+         try (Connection cnx = ds.getConnection()) {
+            Map<String, String> mapMensajes = new HashMap<>();
+            JohnMasterService service = new JohnMasterService(cnx);
+            List<PedidoDetalle> detalles = new ArrayList<PedidoDetalle>();
+            
+            //verifica si se selecciono algun producto
+           String strIdProducto = request.getParameter("lsProducto");
+             if (strIdProducto.isEmpty()) {
+                mapMensajes.put("lsProducto", "Tiene que seleccionar un producto para vender!!");
+            } 
+             String cantidad= request.getParameter("cantidad");
+            
+             if (mapMensajes.isEmpty()) {
+                 
+                PedidoDetalle detalle= new PedidoDetalle();
+               
+                
+                 detalle.setIdProducto(Integer.parseInt(strIdProducto));
+                 detalle.setCantidad(Integer.parseInt(cantidad));
+                 detalles.add(detalle);
+                 
+                 
+             }
+            
+            
+            
+            
+            
+            
+            request.setAttribute("lstProductoDetalle", detalles);
+            request.setAttribute("lsProducto", service.buscarTodosLosProductos());
+            
+
+            request.getRequestDispatcher("/pedidosHome.jsp").forward(request, response);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        
+        
+        
+        
+        
+        
         
     }
     
